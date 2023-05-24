18a96de7d49bcb2f4aad35caaf8cdecb36c595b3
mylesdebastion <42947408+mylesdebastion@users.noreply.github.com>
2023-05-23 18:16:55 -0700

    Update wled_audio_listener.py

--- caption_server/wled_audio_listener.py
+++ caption_server/wled_audio_listener.py
@@ -2,35 +2,57 @@ import socket
 import struct
 import sys
 
-UDP_SYNC_HEADER = b"WLED\0"
+UDP_SYNC_HEADER_V1 = b"00001\0"  # Include the trailing null character
+UDP_SYNC_HEADER_V2 = b"00002\0"  # Include the trailing null character
 UDP_SYNC_PORT = 11988
 
 class WLEDSync:
     def __init__(self):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.socket.bind(("192.168.4.207", UDP_SYNC_PORT))
+        self.socket.bind(("0.0.0.0", UDP_SYNC_PORT))
+        self.socket.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, socket.inet_aton("239.0.0.1") + socket.inet_aton("0.0.0.0"))
     
     def read(self):
         data, _ = self.socket.recvfrom(1024)
-        if len(data) > 5 and data[:6] == UDP_SYNC_HEADER:
-            self.decodeAudioData(data[6:])
-            return True
+        if len(data) > 6:  # The minimum length should be 6 to accommodate the header
+            header = data[:6]  # The header length is 6, including the null character
+            if header == UDP_SYNC_HEADER_V1:
+                self.decodeAudioDataV1(data[6:])
+                return True
+            elif header == UDP_SYNC_HEADER_V2:
+                self.decodeAudioDataV2(data[6:])
+                return True
         return False
     
-    def decodeAudioData(self, audioData):
-        audioSyncPacketFormat = struct.Struct("<fffffff?")
+    def decodeAudioDataV1(self, audioData):
+        audioSyncPacketFormat = struct.Struct("<32BiiifB16Bdd")  # Adjusted format to match documentation
         receivedPacket = audioSyncPacketFormat.unpack(audioData)
         
         # Extract the received audio data values
-        volumeSmth, volumeRaw, sampleRaw, sampleAvg, sampleAgc, rawSampleAgc, multAgc, samplePeak = receivedPacket[:8]
-        fftResult = receivedPacket[8:8+NUM_GEQ_CHANNELS]
-        my_magnitude, FFT_Magnitude, FFT_MajorPeak = receivedPacket[8+NUM_GEQ_CHANNELS:]
+        myVals = receivedPacket[:32]
+        sampleAgc, sampleRaw, sampleAvg, samplePeak = receivedPacket[32:36]
+        fftResult = receivedPacket[36:52]
+        FFT_Magnitude, FFT_MajorPeak = receivedPacket[52:54]
 
-        # Process the received audio data as needed
-        print("Received audio data:")
-        print(f"volumeSmth: {volumeSmth}")
-        print(f"volumeRaw: {volumeRaw}")
-        # ...
+        print("Received audio data (V1 format):")
+        print(f'myVals: {myVals}')
+        print(f'sampleAgc: {sampleAgc}, sampleRaw: {sampleRaw}, sampleAvg: {sampleAvg}, samplePeak: {samplePeak}')
+        print(f'fftResult: {fftResult}')
+        print(f'FFT_Magnitude: {FFT_Magnitude}, FFT_MajorPeak: {FFT_MajorPeak}')
+
+    def decodeAudioDataV2(self, audioData):
+        audioSyncPacketFormat = struct.Struct("<ffBB16Bff")  # Adjusted format to match documentation
+        receivedPacket = audioSyncPacketFormat.unpack(audioData)
+        
+        # Extract the received audio data values
+        sampleRaw, sampleSmth, samplePeak, reserved1 = receivedPacket[:4]
+        fftResult = receivedPacket[4:20]
+        FFT_Magnitude, FFT_MajorPeak = receivedPacket[20:22]
+
+        print("Received audio data (V2 format):")
+        print(f'sampleRaw: {sampleRaw}, sampleSmth: {sampleSmth}, samplePeak: {samplePeak}, reserved1: {reserved1}')
+        print(f'fftResult: {fftResult}')
+        print(f'FFT_Magnitude: {FFT_Magnitude}, FFT_MajorPeak: {FFT_MajorPeak}')
 
 # Example usage
 sync = WLEDSync()
@@ -39,9 +61,8 @@ try:
     print("Script is listening for audio data. Press Ctrl+C to exit.")
     while True:
         if sync.read():
-            # Audio data received, process it
             print("Audio data received!")
-            # ...
 except KeyboardInterrupt:
     print("Exiting the script...")
     sys.exit(0)
+
